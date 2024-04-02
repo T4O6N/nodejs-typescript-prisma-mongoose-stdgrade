9090d1038d55c6b1e92d32c3213c045a69354b71
va1ent0n46 <3t9o9n9@gmail.com>
2024-04-02 10:22:47 +0700

    fix some and explain code

--- config/default.ts
+++ config/default.ts
@@ -2,13 +2,14 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const MONGO_USERNAME = process.env.MONGO_USERNAME || "";
+// pakard - constant, MONGO_USERNAME - (variable), is a Node.js global object that provides - jut, access to the environment variables.
+const MONGO_USERNAME = process.env.MONGO_USERNAME || ""; // This is a logical OR operator
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
 const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.6k5rwjq.mongodb.net/std_grade`;
 
 const SERVER_PORT = process.env.SERVER_PORT
   ? Number(process.env.SERVER_PORT)
-  : 1337;
+  : 1337; // default port.
 
 export const config = {
   mongo: {
